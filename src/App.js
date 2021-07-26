7a035766b9b6f3e7ca69b64b49e6dd630c2bcdb7
David Polcari <dpolcari18@gmail.com>
2021-07-26 18:38:37 -0500

    have apollo client working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react'
+import {useQuery, gql} from '@apollo/client'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const USER_QUERY = gql`query userQuery { user (id: 5) { id, families { id, name } } }`
+
+const App = () => {
+
+  const {data, loading, error} = useQuery(USER_QUERY)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>{data.user}</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
